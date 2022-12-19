00137d85c19e9257c985426c7aa73c3da6893c0f
Bobrikov <bobronaud@gmail.com>
2022-12-19 18:28:38 +0300

    corrent quotes in brain-games.js and cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 const welcome = () => {
-  console.log("Welcome to the Brain Games!");
-  const userName = readlineSync.question("May I have your name? ");
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 
