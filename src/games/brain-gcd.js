7195c6836a0c4ca57b7df671cd91ba09394fe015
Bobrikov <bobronaud@gmail.com>
2023-01-07 12:26:21 +0300

    add calculateGcd function to brain-gcd

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -9,19 +9,26 @@ const generateQuestionValue = () => {
   return `${rundomFirstNumber} ${rundomSecondNumber}`;
 };
 
-const getRightAnswer = (questionValue) => {
-  const numbers = questionValue.split(' ');
-  let firstNumber = numbers[0];
-  let secondNumber = numbers[1];
+const calculateGcd = (firstNumber, secondNumber) => {
   let remainder;
+  let firstDivisor = firstNumber;
+  let secondDivisor = secondNumber;
   for (let i = 0; remainder !== 0; i += 1) {
-    remainder = firstNumber % secondNumber;
-    firstNumber = secondNumber;
+    remainder = firstDivisor % secondDivisor;
+    firstDivisor = secondDivisor;
     if (remainder !== 0) {
-      secondNumber = remainder;
+      secondDivisor = remainder;
     }
   }
-  return secondNumber.toString();
+  return secondDivisor;
+};
+
+const getRightAnswer = (questionValue) => {
+  const numbers = questionValue.split(' ');
+  const firstNumber = numbers[0];
+  const secondNumber = numbers[1];
+  const result = calculateGcd(firstNumber, secondNumber);
+  return result.toString();
 };
 
 const launchBrainGcd = () => launchGame(condition, generateQuestionValue, getRightAnswer);
